30d77a5ae0a85626050539108d47e9976f20d5d5
CookieMC337 <julienmichell2003@gmail.com>
2022-10-08 00:18:57 +0200

    Fixed Auth Error

--- src/MyLocAPI.php
+++ src/MyLocAPI.php
@@ -31,11 +31,21 @@ class MyLocAPI
      * @param string $token API Token for all requests
      * @param null $httpClient
      */
-    public function __construct(string $token, $httpClient = null)
-    {
+    public function __construct(string $token, $httpClient = null) {
         $this->apiToken = $token;
         $this->setHttpClient($httpClient);
         $this->setCredentials($token);
+    }
+
+
+    public function setHttpClient(Client $httpClient = null)
+    {
+        $this->httpClient = $httpClient ?: new Client([
+            'allow_redirects' => false,
+            'follow_redirects' => false,
+            'timeout' => 120,
+            'http_errors' => false,
+        ]);
     }
 
     public function setCredentials($credentials)
@@ -45,6 +55,114 @@ class MyLocAPI
         }
 
         $this->credentials = $credentials;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getHttpClient(): Client
+    {
+        return $this->httpClient;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->apiToken;
+    }
+    
+
+    /**
+     * @return Credentials
+     */
+    private function getCredentials(): Credentials
+    {
+        return $this->credentials;
+    }
+
+
+    /**
+     * @param string $actionPath The resource path you want to request, see more at the documentation.
+     * @param array $params Array filled with request params
+     * @param string $method HTTP method used in the request
+     *
+     * @return ResponseInterface
+     *
+     * @throws ParameterException If the given field in params is not an array
+     */
+    private function request(string $actionPath, array $params = [], string $method = 'GET'): ResponseInterface
+    {
+        $url = $this->getCredentials()->getUrl() . $actionPath;
+
+        if (!is_array($params)) {
+            throw new ParameterException();
+        }
+
+        $params['Authorization'] = 'Bearer ' . $this->apiToken;
+
+        switch ($method) {
+            case 'GET':
+                return $this->getHttpClient()->get($url, ['verify' => false, 'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->apiToken]]);
+            case 'POST':
+                return $this->getHttpClient()->post($url, ['verify' => false, 'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->apiToken], 'form_params' => $params,]);
+            case 'PUT':
+                return $this->getHttpClient()->put($url, ['verify' => false, 'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->apiToken], 'form_params' => $params,]);
+            case 'DELETE':
+                return $this->getHttpClient()->delete($url, ['verify' => false, 'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->apiToken], 'form_params' => $params,]);
+            case 'PATCH':
+                return $this->getHttpClient()->patch($url, ['verify' => false, 'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->apiToken], 'form_params' => $params,]);
+            default:
+                throw new ParameterException('Wrong HTTP method passed');
+        }
+    }
+
+    private function processRequest(ResponseInterface $response)
+    {
+        $response = $response->getBody()->__toString();
+        $result = json_decode($response);
+        if (json_last_error() == JSON_ERROR_NONE) {
+            return $result;
+        } else {
+            return $response;
+        }
+    }
+
+
+    public function get($actionPath, $params = [])
+    {
+        $response = $this->request($actionPath, $params);
+
+        return $this->processRequest($response);
+    }
+
+    public function post($actionPath, $params = [])
+    {
+        $response = $this->request($actionPath, $params, 'POST');
+
+        return $this->processRequest($response);
+    }
+
+    public function put($actionPath, $params = [])
+    {
+        $response = $this->request($actionPath, $params, 'PUT');
+
+        return $this->processRequest($response);
+    }
+
+    public function delete($actionPath, $params = [])
+    {
+        $response = $this->request($actionPath, $params, 'DELETE');
+
+        return $this->processRequest($response);
+    }
+
+    public function patch($actionPath, $params = [])
+    {
+        $response = $this->request($actionPath, $params, 'PATCH');
+
+        return $this->processRequest($response);
     }
 
     /**
@@ -99,120 +217,6 @@ class MyLocAPI
     {
         if (!$this->whmcsHandler) $this->whmcsHandler = new WHMCS($this);
         return $this->whmcsHandler;
-    }
-
-
-
-    /**
-     * @param string $actionPath The resource path you want to request, see more at the documentation.
-     * @param array $params Array filled with request params
-     * @param string $method HTTP method used in the request
-     *
-     * @return ResponseInterface
-     *
-     * @throws ParameterException If the given field in params is not an array
-     */
-    private function request(string $actionPath, array $params = [], string $method = 'GET'): ResponseInterface
-    {
-        $url = $this->getCredentials()->getUrl() . $actionPath;
-
-        if (!is_array($params)) {
-            throw new ParameterException();
-        }
-
-        $params['Authorization'] = 'Bearer ' . $this->apiToken;
-
-        switch ($method) {
-            case 'GET':
-                return $this->getHttpClient()->get($url, ['verify' => false, 'query' => $params,]);
-            case 'POST':
-                return $this->getHttpClient()->post($url, ['verify' => false, 'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->apiToken,], 'form_params' => $params,]);
-            case 'PUT':
-                return $this->getHttpClient()->put($url, ['verify' => false, 'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->apiToken,], 'form_params' => $params,]);
-            case 'DELETE':
-                return $this->getHttpClient()->delete($url, ['verify' => false, 'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->apiToken,], 'form_params' => $params,]);
-            case 'PATCH':
-                return $this->getHttpClient()->patch($url, ['verify' => false, 'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->apiToken,], 'form_params' => $params,]);
-            default:
-                throw new ParameterException('Wrong HTTP method passed');
-        }
-    }
-
-    /**
-     * @return Credentials
-     */
-    private function getCredentials(): Credentials
-    {
-        return $this->credentials;
-    }
-
-    /**
-     * @param $response ResponseInterface
-     *
-     * @return array|string
-     */
-    private function processRequest(ResponseInterface $response)
-    {
-        $response = $response->getBody()->__toString();
-        $result = json_decode($response);
-        if (json_last_error() == JSON_ERROR_NONE) {
-            return $result;
-        } else {
-            return $response;
-        }
-    }
-
-    /**
-     * @return Client
-     */
-    public function getHttpClient(): Client
-    {
-        return $this->httpClient;
-    }
-
-    /**
-     * @param $httpClient Client|null
-     */
-    public function setHttpClient(Client $httpClient = null)
-    {
-        $this->httpClient = $httpClient ?: new Client(['allow_redirects' => false, 'follow_redirects' => false, 'timeout' => 120, 'http_errors' => false,]);
-    }
-
-
-
-    public function get($actionPath, $params = [])
-    {
-        $response = $this->request($actionPath, $params);
-
-        return $this->processRequest($response);
-    }
-
-    public function post($actionPath, $params = [])
-    {
-        $response = $this->request($actionPath, $params, 'POST');
-
-        return $this->processRequest($response);
-    }
-
-    public function put($actionPath, $params = [])
-    {
-        $response = $this->request($actionPath, $params, 'PUT');
-
-        return $this->processRequest($response);
-    }
-
-    public function delete($actionPath, $params = [])
-    {
-        $response = $this->request($actionPath, $params, 'DELETE');
-
-        return $this->processRequest($response);
-    }
-
-    public function patch($actionPath, $params = [])
-    {
-        $response = $this->request($actionPath, $params, 'PATCH');
-
-        return $this->processRequest($response);
     }
 
 }
